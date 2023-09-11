e072bbbfcdd2c7cc44cf05fac8abaa28b0a6437a
gurge <gurge@DESKTOP-RIK2PQ7>
2023-09-11 03:07:29 -0300

    adicionando usuarios via identity

---
+++ UsuariosApi/Dtos/CreateUsuarioDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuarioApi.Dtos
+{
+    public class CreateUsuarioDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
